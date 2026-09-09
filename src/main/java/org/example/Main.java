592790046956ef0e98a175c342e34462c0b2b39a
viurote <dmitriy.kst2@gmail.com>
2026-09-09 21:15:42 +1000

    EmployeeRecord,Proffesion. Доработка main, генерация списка records по ТЗ/логирование

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,69 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import net.datafaker.Faker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+//{ "id": UUID,
+// "name": string,
+// "age" : int,
+// "salary" : int,
+// "PROFESSION": (ARTIST,IT,ACCOUNTANT) }
+
+public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static Faker faker = new Faker();
+
+    public static void main(String[] args) {
+
+        if (args.length < 1) {
+            log.error("Target directory is not specified! Usage: java Main <output_dir_path>");
+            System.exit(1);
         }
-    }
+
+        File outputDir = new File(args[0]);
+
+        if (!outputDir.exists()) {
+            boolean created = outputDir.mkdirs();
+
+            if (created) {
+                log.info("Created directory: {}",outputDir.getAbsolutePath());
+            } else {
+                log.error("Failed to create directory: {}", outputDir.getAbsolutePath());
+                System.exit(1);
+            }
+        } else if(!outputDir.isDirectory()) {
+            log.error("Specified path is not a directory: {}", outputDir.getAbsolutePath());
+            System.exit(1);
+        }
+
+        log.info("Files will be saved to: {}", outputDir.getAbsolutePath());
+
+
+        int recordsCount = faker.number().numberBetween(10,101);
+
+        List<EmployeeRecord> records = new ArrayList<>();
+
+        for (int i = 0; i < recordsCount; i++){
+            records.add(generateRandomEmployee(faker));
+        }
+        log.info("Generated records: {}",recordsCount);
+        log.info("Records List: {}",records);
+        }
+
+        private static EmployeeRecord generateRandomEmployee(Faker faker) {
+        return new EmployeeRecord(
+                UUID.randomUUID(),
+                faker.name().fullName(),
+                faker.number().numberBetween(18,65),
+                faker.number().numberBetween(1500,7500),
+                faker.options().option(Profession.class)
+        );
+        }
 }
---
+++ src/main/java/org/example/Profession.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public enum Profession {
+    ARTIST,
+    IT,
+    ACCOUNTANT
+}
