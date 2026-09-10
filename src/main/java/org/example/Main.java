cdf653ed9507cbaef70d94e9a7a15515911ab959
viurote <dmitriy.kst2@gmail.com>
2026-09-10 21:03:42 +1000

    reset

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,24 @@
 package org.example;
 
-import org.example.analytics.AnalyticsRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.nio.file.Path;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) throws Exception {
-        if (args.length < 4) {
-            System.err.println("Usage: java -jar analytics.jar <inputDir> <outputDir> <emailToken> <toEmail>");
-            System.exit(1);
-        }
+    public static void main(String[] args) {
+        String content = """
+                File content
+                """;
 
-        Path inputDir  = Path.of(args[0]);
-        Path outputDir = Path.of(args[1]);
-        String emailToken = args[2];
-        String toEmail    = args[3];
+        byte[] fileBytes = content.getBytes(StandardCharsets.UTF_8);
 
-        log.info("Starting analytics. inputDir={}, outputDir={}, toEmail={}", inputDir, outputDir, toEmail);
-
-        AnalyticsRunner runner = new AnalyticsRunner(inputDir, outputDir, emailToken, toEmail);
-        runner.run();
+        EmailNotificationService emailNotificationService = new EmailNotificationService("TOKEN!");
+        emailNotificationService.sendEmail("isicjua@gmail.com", "body text", fileBytes);
     }
-}
+
+}
